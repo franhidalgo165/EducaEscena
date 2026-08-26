db52f5c61025d00c4fa8a7b4ae1bb366ba4c7dcc
HIDALGO-DEV <fhggamesepic99@gmail.com>
2026-08-26 13:36:11 +0200

    mapa_evento_individual

--- SHORTCODES_PHP/mapa_evento.php
+++ SHORTCODES_PHP/mapa_evento.php
@@ -1,7 +1,16 @@
 add_shortcode('mi_mapa_evento', function() {
     $venue_id = tribe_get_venue_id();
     if ($venue_id) {
-        return tribe_get_embedded_map($venue_id);
+        
+        // Filtro temporal para modificar el zoom que pide The Events Calendar a Google
+        add_filter('tribe_get_basic_gmap_embed_url_args', function($args) {
+            $args['zoom'] = 17; 
+            return $args;
+        });
+
+        $map = tribe_get_embedded_map($venue_id);
+        
+        return '<div class="mi-mapa-personalizado">' . $map . '</div>';
     }
     return 'No hay ubicación definida.';
 });
